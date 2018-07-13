ad873fa520cc09c4c5eb2892d2f3f91ff22c17a0
JackieMium <jiangjun12321@hotmmail.com>
2018-07-13 15:32:25 +0800

    syntax and dir change

--- GSE42872/step1_getGEO.R
+++ GSE42872/step1_getGEO.R
@@ -1,13 +1,15 @@
 if(FALSE){
   library(GEOquery)
-  eSet <- getGEO('GSE42872', destdir=".",
-                 AnnotGPL = F,
-                 getGPL = F)
-  save(eSet,file='GSE42872_eSet.Rdata')
+  eSet <- getGEO('GSE42872', destdir="raw_data/",
+                 AnnotGPL = FALSE,
+                 getGPL = FALSE)
+  save(eSet,file='output_data/GSE42872_eSet.Rdata')
 }
-load('GSE42872_eSet.Rdata')
+load('output_data/GSE42872_eSet.Rdata')
 b = eSet[[1]]
 raw_exprSet=exprs(b) 
 group_list=c(rep('control',3),rep('case',3))
+
+# not run since files are already there
 save(raw_exprSet,group_list,
-     file='GSE42872_raw_exprSet.Rdata')
+     file='output_data/GSE42872_raw_exprSet.Rdata')
